ac1ac68aae3b75ef721bac38a3780cca92fdf865
Sagnik-Bankura <141158542+Sagnik-Bankura@users.noreply.github.com>
2023-08-02 12:31:55 +0530

    Add files via upload

---
+++ Odd-Even.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Enter The Number " ;
+    cin >> a;
+    cout <<  endl;
+    if(a%2==0)
+    {
+        cout << "Number is Even " << endl;
+    }
+    else
+    {
+        cout << "Number Is Odd" << endl;
+    }
+    return 0;
+}
+    
